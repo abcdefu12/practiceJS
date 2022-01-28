f84dcde6e34c4f28c92eb205d3bcbfea15ad672f
Gim <gbks081@gmail.com>
2022-01-28 14:26:22 +0900

    feat: coin-tracker-react [TIL]
    get API to show coins that exist
    .
    can be added translator like conversion factor

---
+++ coin-tracker-react/src/App.js
@@ -0,0 +1,34 @@
+import {useState, useEffect} from "react";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+
+
+  },  []);
+  return (
+    <div className="App">
+      <h1>The Coins total: {loading ? "" : `${coins.length}`}</h1>
+      {loading ? <stroing>Loading...</stroing> :
+      <select>
+        {coins.map((coin)=> 
+          <option>
+            {coin.name} 
+            ({coin.symbol}):${coin.quotes.USD.price}
+          </option>
+        )}
+      </select>}
+      
+      
+    </div>
+  );
+}
+
+export default App;
