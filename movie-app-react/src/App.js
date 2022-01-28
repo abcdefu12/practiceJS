b8cca87f71e229e6082c2cbe7eea2d43fa7f092f
Gim <gbks081@gmail.com>
2022-01-29 00:42:33 +0900

    feat: add react-router-dom [TIL]
    BrowserRouter Router Switch Route Link
    npm i react-router-dom@5.3.0
    check the version of react-router-dom
    use switch to render one at once

--- movie-app-react/src/App.js
+++ movie-app-react/src/App.js
@@ -4,14 +4,41 @@ import Movie from "./components/Movie"
 import './App.css';
 import Home from "./routes/Home"
 import Detail from "./routes/Detail"
+// import React from "react";
+import {BrowserRouter as Router,
+  Switch, Route, } from "react-router-dom";
+
 
 function App() {
   return(
     <div>
-      
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/movie">
+            <Detail />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>   
     </div>
   );
 };
 
 export default App;
+
+
+/* <Switch></Switch>
+use switch to render one at one time
+react router can render in double at once if i want to */
+
+
+// react-router-dom v6.2.1
+// <BrowserRouter>
+//   <Routes>
+//     <Route exact path="/" element={<Home />}></Route>
+//     <Route path="/movie" element={<Detail />}></Route>
+//   </Routes>
+// </BrowserRouter>
