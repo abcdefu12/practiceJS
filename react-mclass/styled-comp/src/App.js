e4665f375e8618c3a4ff7ad452f4ff441b8d1395
Gim <gbks081@gmail.com>
2022-03-05 23:29:27 +0900

    feat: base of styled-components

--- react-mclass/styled-comp/src/App.js
+++ react-mclass/styled-comp/src/App.js
@@ -1,5 +1,33 @@
+import styled from "styled-components";
+
+//def
+const Father = styled.div`
+  display: flex;
+`
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
+
+//imp
 function App() {
-  return null;
+  return(
+    <Father>
+      <BoxOne>
+        <Text>HELLO</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
+  );
 }
 
 export default App;
