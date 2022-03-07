e18ea3bfba4499e12ae8e5aeb4da06b6184b640f
Gim <gbks081@gmail.com>
2022-03-08 00:21:35 +0900

    feat: anim of styled-comp basic

--- react-mclass/styled-comp/src/App.js
+++ react-mclass/styled-comp/src/App.js
@@ -1,15 +1,42 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 //def
 const Father = styled.div`
   display: flex;
 `;
+const rotateAnim = keyframes`
+  /* from{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  to{
+    transform: rotate(360deg);
+    border-radius: 70px;
+  } */
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    border-radius: 70px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
 const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
+  animation: ${rotateAnim} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    font-size: 36px;
+  }
 `;
-
 const Text = styled.span`
   color: white;
 `;
@@ -22,21 +49,25 @@ const Btn = styled.button`
   border-radius: 15px;
   height: 50px;
 `;
-const Input = styled.input.attrs({required: true, minLength: 5})`
+const Input = styled.input.attrs({ required: true, minLength: 5 })`
   background-color: blueviolet;
 `;
 
 //imp
 function App() {
-  return(
+  return (
     <Father as="header">
       <Box bgColor="teal">
         <Text>HELLO</Text>
       </Box>
       <Circle bgColor="whitesmoke" />
-      <Box bgColor="tomato" />
+      <Box bgColor="tomato">
+        <span>😍</span>
+      </Box>
       <Btn>Log in</Btn>
-      <Btn as="a" href="/">Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
       <Input />
     </Father>
   );
