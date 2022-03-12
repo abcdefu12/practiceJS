c5240e69b27d7238dd562a7e4112ebe5d10001e9
Gim <gbks081@gmail.com>
2022-03-12 16:30:23 +0900

    feat: react form event

--- react-mclass/ts-react/src/App.tsx
+++ react-mclass/ts-react/src/App.tsx
@@ -1,14 +1,33 @@
+import React, {useState} from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./circle";
 
 //def
 
 //imp
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    // console.log(event.currentTarget.value)
+    const{
+      currentTarget: {value},
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // console.log("hello",value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="brown"/>
-      <Circle bgColor="tomato" text="circle text"/>
+      <form onSubmit={onSubmit}>
+        <input 
+          value={value}
+          onChange={onChange}
+          type="text" 
+          placeholder="username"
+        />
+        <button>log in</button>
+      </form>
     </div>
   );
 }
